50d0cf8524cfbac54f46f4572c0018969e8a8dd5
xscriptz <athallur@iu.edu>
2016-08-28 20:16:38 -0400

    second brut

---
+++ bruteforce/pass_algo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	string key;
+	cout << "Enter the password: " << endl;
+	cin >> key;
+	cout << key << endl;
+	key_cracker(key);
+	return 0;
+}
+void key_cracker(string key)
+{
+	int num[] = {1,2,3,4,5,6,7,8,9,0};
+	char charact = 'a';
+	for()
+}
